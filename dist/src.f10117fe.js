85ab221c19f6508e1c2f8873891ecedb91d180c2
Raphael <raphaelaquino23@gmail.com>
2021-11-03 14:02:58 +0800

    commit on eventing

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -2232,7 +2232,6 @@ var axios_1 = __importDefault(require("axios"));
 var User = function () {
   function User(data) {
     this.data = data;
-    this.events = {};
   }
 
   User.prototype.get = function (propName) {
@@ -2241,24 +2240,6 @@ var User = function () {
 
   User.prototype.set = function (update) {
     Object.assign(this.data, update);
-  };
-
-  User.prototype.on = function (eventName, callback) {
-    var handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  };
-
-  User.prototype.trigger = function (eventName) {
-    var handlers = this.events[eventName];
-
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-
-    handlers.forEach(function (callback) {
-      callback();
-    });
   };
 
   User.prototype.fetch = function () {
@@ -2295,7 +2276,7 @@ var User_1 = require("./models/User");
 
 var user = new User_1.User({});
 user.set({
-  name: 'New Name',
+  name: 'New Record',
   age: 9999
 });
 user.save();
@@ -2327,7 +2308,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1067" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "14284" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { User } from './models/User';
 
 const user = new User({});
 
-user.set({ name: 'New Name', age: 9999});
+user.set({ name: 'New Record', age: 9999});
 
 user.save();
