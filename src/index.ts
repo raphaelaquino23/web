2261cb8bb8aeec0620d781e1af3d2cb7ddc503ee
Raphael <raphaelaquino23@gmail.com>
2021-11-13 10:17:55 +0800

    creation of buildUser and expanding Model

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'new user 2', age: 0});
-
-class Person {
-    constructor ( public firstName: string, public lastName: string){}
-
-    get fullName(): string {
-        return `${this.firstName} ${this.lastName}`
-    }
-}
-
-console.log(user.get('name'));
+const user = User.buildUser({ id: 1 });
 
 user.on('change', () => {
-    console.log('User was changed')
-})
+    console.log(user);
+});
 
-user.save();
+user.fetch();
