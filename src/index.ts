9f7cd079a5c2c8b8184f2e92d3a84c40cba95c2b
Raphael <raphaelaquino23@gmail.com>
2021-11-20 16:14:09 +0800

    Added View with abstract methods

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import { UserForm } from "./views/UserForm";
+import { User } from "./models/User";
 
-const userForm = new UserForm(
-  document.getElementById('root')
-);
+const user = User.buildUser({ name: "Users NAME", age: 20 });
 
-userForm.render(); 
+const root = document.getElementById("root")
+
+if(root){
+  const userForm = new UserForm(root, user);
+  userForm.render();
+}else {
+  throw new Error("Root element not found");
+}
+
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,42 +1,41 @@
-export class UserForm {
-    constructor(public parent: Element){}
+import { User, UserProps } from "../models/User";
+import { View } from "./View";
 
+export class UserForm extends View<User, UserProps>{
     eventsMap(): { [key:string]: () => void }{
         return {
-            'click:button': this.onButtonClick
+            'click:.set-age': this.onSetAgeClick,
+            'click:.set-name': this.onSetNameClick,
+            'click:.save-model': this.onSaveClick
         };
-    }
+    };
 
-    onButtonClick(): void{
-        console.log('Clicked');
-    }
+    onSetAgeClick = (): void => {
+        this.model.setRandomAge();
+    };
+
+    onSaveClick = (): void => {
+        this.model.save();
+    };
+
+    onSetNameClick = (): void => {
+        const input = this.parent.querySelector('input');
+
+        if(input){
+            const name = input.value;
+            this.model.set({ name });
+        }
+    };
 
     template(): string{
         return `
         <div>
             <h1>User Form </h1>
-            <input /> 
+            <input placeholder = "${this.model.get('name')}"/> 
+            <button class="set-name">Change Name</button>
+            <button class="set-age">Click for Random Age</button>
+            <button class ="save-model">Save User</button>
         </div>
         `;
-    }
-
-    bindEvents(fragment: DocumentFragment): void{
-        const eventsMap = this.eventsMap(); 
-
-        for(let eventKey in eventsMap){
-            const [eventName, selector] = eventKey.split(':');
-
-            fragment.querySelectorAll(selector).forEach(element => {
-                element.addEventListener(eventName, eventsMap[eventKey])
-            }); 
-
-        }
-    }
-
-    render(): void{
-        const templateElement = document.createElement('template');
-        templateElement.innerHTML = this.template(); 
-
-        this.parent.append(templateElement.content);
-    }
-}
+    };
+};
